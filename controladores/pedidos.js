e7314629fab9e03ba3e4f4dcb4c5d0814cc95121
Yuendel Farias <yuendel.fb@hotmail.com>
2021-05-12 09:41:06 -0300

    Carrinho sem limpar o código

--- controladores/pedidos.js
+++ controladores/pedidos.js
@@ -1,6 +1,9 @@
 const fs = require('fs/promises');
 
 
+
+
+
 const lerProdutos = async (req, res) => {
     const { categoria, precoInicial, precoFinal } = req.query;
 
@@ -32,11 +35,89 @@ const lerProdutos = async (req, res) => {
     }
 
 }
+const lerCarrinho = async (req, res) => {
+    const carrinho = (await fs.readFile('carrinho.json')).toString();
+    if (carrinho.dataDeEntrega === "") {
+        res.json({
+            "produtos": [],
+            "subtotal": 0,
+            "dataDeEntrega": null,
+            "valorDoFrete": 0,
+            "totalAPagar": 0
+        })
+
+    } else {
+        const novo = (await fs.readFile('carrinho.json')).toString();
+        const novoCarrinho = JSON.parse(novo);
+
+        res.json(novoCarrinho)
+    }
+
+}
+
+const adicionarProdutoCarrinho = async (req, res) => {
+    const body = req.body;
+    const carrinho = (await fs.readFile('carrinho.json')).toString();
+    const novoCarrinho = JSON.parse(carrinho);
+    const dataAtual = new Date();
+
+    const produtos = (await fs.readFile("data.json")).toString();
+    const dadosProdutos = JSON.parse(produtos);
+    const dadosFiltrados = dadosProdutos.produtos.filter(x => x.estoque > 0);
+    const produto = dadosFiltrados.find(x => x.id === body.id);
+    if (produto.estoque < body.quantidade) {
+        res.json('Quantidade insuficiente para adição no carrinho')
+
+    } else {
+        const verificarIdProduto = novoCarrinho.produtos.findIndex(x => x.id === body.id);
+
+        if (verificarIdProduto !== -1) {
+            if (novoCarrinho.produtos[verificarIdProduto].quantidade + body.quantidade > produto.estoque) {
+                res.json(`Estoque insuficiente de ${novoCarrinho.produtos[verificarIdProduto].nome} para adicionar ao carrinho`)
+            } else {
+                novoCarrinho.produtos[verificarIdProduto].quantidade += body.quantidade;
+                novoCarrinho.subTotal += produto.preco;
+                novoCarrinho.dataDeEntrega = new Date(dataAtual.setDate(dataAtual.getDate() + 15));
+                if (novoCarrinho.subTotal <= 20000) {
+                    novoCarrinho.valorDoFrete = 5000;
+                }
+                novoCarrinho.totalAPagar = novoCarrinho.subTotal + novoCarrinho.valorDoFrete;
+                await fs.writeFile('carrinho.json', JSON.stringify(novoCarrinho))
+                res.json(novoCarrinho);
+            }
+
+        } else {
+            novoCarrinho.produtos.push({
+                "id": produto.id,
+                "quantidade": body.quantidade,
+                "nome": produto.nome,
+                "preco": produto.preco,
+                "categoria": produto.categoria
+            })
+            novoCarrinho.subTotal += produto.preco;
+            novoCarrinho.dataDeEntrega = new Date(dataAtual.setDate(dataAtual.getDate() + 15));
+            if (novoCarrinho.subTotal <= 20000) {
+                novoCarrinho.valorDoFrete = 5000;
+
+            }
+            novoCarrinho.totalAPagar = novoCarrinho.subTotal + novoCarrinho.valorDoFrete;
+            await fs.writeFile('carrinho.json', JSON.stringify(novoCarrinho))
+            res.json(novoCarrinho);
+        }
+
+
+
+
+
+    }
+}
 
 
 
 
 
 module.exports = {
-    lerProdutos
+    lerProdutos,
+    lerCarrinho,
+    adicionarProdutoCarrinho
 }
--- rotas.js
+++ rotas.js
@@ -2,6 +2,8 @@ const express = require('express');
 const rotas = express();
 const pedidos = require('./controladores/pedidos');
 
-rotas.get('/produtos', pedidos.lerProdutos)
+rotas.get('/produtos', pedidos.lerProdutos);
+rotas.get('/carrinho', pedidos.lerCarrinho);
+rotas.post('/carrinho/produtos', pedidos.adicionarProdutoCarrinho);
 
 module.exports = rotas;
